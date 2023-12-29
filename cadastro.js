1e5ce4bf428e6290568c9f02f196ce73d1d3fbb3
Isaac-Gomes-085 <isaacgomes932@gmail.com>
2023-12-29 18:55:45 -0300

    Um simples sistema de login

---
+++ cadastro.js
@@ -0,0 +1,6 @@
+/*const nome = prompt(alert('Como se chama:'))
+alert('Seja bem vindo(a), ' + nome + '!')*/
+
+/*const res = confirm('Dejesa enviar o formulário?');
+
+let resultado = res ? alert(Sim) : alert(Não);*/
