d756300c9c05a483ae564b2b634deeead9e35168
Isaac Gomes <118241033+Isaac-Gomes-085@users.noreply.github.com>
2024-01-04 23:47:56 -0300

    Delete cadastro.js

--- cadastro.js
+++
@@ -1,6 +0,0 @@
-/*const nome = prompt(alert('Como se chama:'))
-alert('Seja bem vindo(a), ' + nome + '!')*/
-
-/*const res = confirm('Dejesa enviar o formulário?');
-
-let resultado = res ? alert(Sim) : alert(Não);*/
